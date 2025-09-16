d007ed1d64b02ead8403b9543af6b03587b0aa18
GigiusB <g.bronzini@gmail.com>
2025-09-16 08:01:25 +0100

    Fix #4 Taiga implementation

--- src/issues/backends/taiga.py
+++ src/issues/backends/taiga.py
@@ -16,7 +16,7 @@ class Backend(BaseBackend):
     def get_issue_choices(self) -> list[tuple[str, str]]:
         from issues.config import CONFIG
 
-        return list((x, y) for x, y in CONFIG.TYPES.items())
+        return list(CONFIG.TYPES.items())
 
     def create_ticket(self, cleaned_data: "IssueFormCleanedData") -> bool:
         description = self.get_description({**cleaned_data, "screenshot_url": ""})
